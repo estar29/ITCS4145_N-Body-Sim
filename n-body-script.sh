fabc15d956f9e917f829d2d89d2080b92c8ec4a6
Evan Stark <estark3@charlotte.edu>
2025-02-11 23:17:10 -0500

    Adding slurm script for Centaurus.

---
+++ n-body-script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Evan Stark - February 11th 2025 - ITCS 4145 001
+# Slurm script to benchmark n-body-sim.cpp on Centaurus.
+
+#SBATCH --partition=Centaurus
+#SBATCH --mem=16GB
+#SBATCH --time=02:00:00
+
+g++ -o n-body-output n-body-sim.cpp
+./output 8 200 5000000 10000
+./output 100 1 10000 100
+./output 1000 1 10000 100
