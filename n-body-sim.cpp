64ed4eae7ab43338c3d6bfd41ae9a4825ca257cd
Evan Stark <estark3@charlotte.edu>
2025-02-11 23:06:00 -0500

    Finished  n-body simulation without feeding python script.

--- n-body-sim.cpp
+++ n-body-sim.cpp
@@ -10,6 +10,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <math.h>
+#include <random>
 
 using namespace std;
 
@@ -17,45 +18,7 @@ using namespace std;
 double grav = 0.00000000006674;
 double soft_factor = 0.000000000001;
 
-void calc_force(particle arr[]) 
-{
-    int arr_size = sizeof(arr) / sizeof(particle);
-
-    // 
-    for (int i=0; i < arr_size; i++) {
-
-        for (int j=0; j < arr_size; j++) {
-            // Prevent interactions to itself.
-            if (i != j) {
-                double loc_diff = std::abs(arr[i].position - arr[j].position);
-                double force = (grav * arr[i].mass * arr[j].mass) / pow(loc_diff, 2);
-                arr[i].force += force_direction(arr[i].position, arr[j].position, force);
-            }
-        }
-
-    }
-
-}
-
-// Calculating the direction of the force.
-// Parameters: locations of the two particles and the gross force they make.
-double force_direction(double loc1, double loc2, double force)  
-{
-    double location_diff = std::abs(loc1 - loc2) + soft_factor;
-    double direction = ( (loc1 - loc2) / location_diff) * force;
-    return direction;
-}
-
-// AVP = acceleration, velocity, positions.
-// ALL PARAMS: accel, direction, mass, new_vel, old_vel, time_step, new_pos, old_pos
-void calc_new_AVP(double direction, double mass, double old_vel, double time_step_size, double old_pos) 
-{
-    double accel = direction / mass;
-    double new_vel = old_vel + (accel * time_step_size);
-    double new_pos = old_pos + (new_vel * time_step_size);
-}
-
-// Initialize the particle 
+// Initialize particle parameters.
 struct particle 
 {
     double mass;
@@ -63,6 +26,33 @@ struct particle
     double velocity;
     double force;
 };
+
+double calc_force(particle p1, particle p2) 
+{
+    double loc_diff = std::abs(p1.position - p2.position);
+    double force = (grav * p1.mass * p2.mass) / pow(loc_diff, 2);
+    return force;
+}
+
+// Calculating the direction of the force.
+double force_direction(particle p1, particle p2, double force)  
+{
+    double location_diff = std::abs(p1.position - p2.position) + soft_factor;
+    double direction = ( (p1.position - p2.position) / location_diff) * (force);
+    return direction;
+}
+
+// AVP = acceleration, velocity, positions.
+double* calc_new_AVP(particle p, double time_step_size) 
+{
+    double* output_array = (double*) malloc(2 * sizeof(double) );
+    double accel = p.force / p.mass;
+    // New velocity value.
+    output_array[0] = p.velocity + (accel * time_step_size);
+    // New position value.
+    output_array[1] = p.position + (p.velocity * time_step_size);
+    return output_array;
+}
 
 // Predefined scenario for testing purposes.
 void test_case(particle arr[]) 
@@ -77,40 +67,95 @@ void test_case(particle arr[])
     arr[2] = p3;
 }
 
+particle* generate_data (particle arr[], int num_particles) 
+{
+    // Ensure random number generation.
+    srand(time(0));
+    
+    for(int i=0; i < num_particles; i++) {
+        arr[i].mass = rand() / 100;
+        arr[i].position = rand() / 100;
+        arr[i].velocity = rand() / 100;
+
+        // Random chance to produce a negative number.
+        int neg = rand();
+        if (neg % 2 == 1){
+            arr[i].position = arr[i].position - (2 * arr[i].position);
+            arr[i].velocity = arr[i].velocity - (2 * arr[i].position);
+        }
+
+        std::cout << "\n" << arr[i].mass << " " << arr[i].position << " " << arr[i].velocity << " ";
+    }
+    std::cout << "\n";
+    return arr;
+}
+
 int main (int argc, char* argv[]) 
 {
     // Initialize the command line arguments.
+    int num_particles = std::atoi(argv[1]);
+    int time_step_size = std::atoi(argv[2]);
+    int iterations = std::atoi(argv[3]);
+    int dump_rate = std::atoi(argv[4]);
 
-    // int num_particles = std::atoi(argv[1]);
-    // int time_step_size = std::atoi(argv[2]);
-    // int iterations = std::atoi(argv[3]);
-    // int dump_rate = std::atoi(argv[4]);
+    struct particle* arr = (particle*) malloc(num_particles * sizeof(particle));
 
-    // TEST CASE!
-    int num_particles = 3;
-    struct particle arr[num_particles];
-    struct particle* ptr = (particle*) malloc(sizeof(particle) * num_particles);
-
-    // Print out if malloc fails.
-    if (ptr == NULL) {
-        std::cout << "Malloc failed!  Exiting program.";
+    // Ensure malloc succeeds.
+    if (arr == NULL) {
+        std::cout << "Malloc failed for arr!";
         return -1;
     }
 
-    test_case(arr);
+    arr = generate_data(arr, num_particles);
 
-    // Reset all the particle's forces.
-    for (int f=0; f < num_particles; f++) {
-        arr[f].force = 0;
-        std::cout << arr[f].force << " ";
+    int iteration_count = 0;
+    // Run the program for x number of iterations.
+    while(iteration_count <= iterations) 
+    {
+        // Reset all the particle's forces.
+        for (int f=0; f < num_particles; f++) {
+            arr[f].force = 0;
+        }
+
+        // Calculate forces between each combination of particles.
+        for (int i=0; i < num_particles; i++) {
+
+            for (int j=0; j < num_particles; j++) {
+                // Prevent interactions to itself.
+                if (i != j) {
+                    double temp_force = calc_force(arr[i], arr[j]);
+                    arr[i].force += force_direction(arr[i], arr[j], temp_force);
+                }
+            }
+
+            // After all forces added up, update acceleration, velocity, position.
+            double* new_values =(double*) malloc(2 * sizeof(double) );
+
+            if(new_values == NULL) {
+                std::cout << "Malloc failed for new_values!";
+                return -1;
+            }
+
+            new_values = calc_new_AVP(arr[i], time_step_size);
+            arr[i].velocity += new_values[0];
+            arr[i].position += new_values[1];
+            free(new_values);
+        }
+        
+        // For every [dump_rate] interval, print the output to the log file.
+        if (iteration_count % dump_rate == 0) {
+            for (int a=0; a < num_particles; a++) {
+                std::cout << arr[a].mass << " " << arr[a].position << " " << arr[a].velocity << " " << arr[a].force << " ";
+            }
+            
+            // Buffer line for formatting.
+            std::cout << "\n";
+        }
+        
+        iteration_count++;
     }
 
-    // Check if output worked.
-    // for (int a=0; a < 3; a++) {
-    //  std::cout << arr[a].mass << " " << arr[a].position << " " << arr[a].velocity << " ";
-    //}
-
     // Deallocating and terminating.
-    free(ptr);
+    free(arr);
     return 0;
 }
